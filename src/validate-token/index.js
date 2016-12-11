53ceda613eb5f99ee0d51537cd667bbe67c208c3
Luca Tabone <taboneluca3@gmail.com>
2016-12-11 11:51:55 +0100

    Move token validation logic in its own module

---
+++ src/validate-token/index.js
@@ -0,0 +1,15 @@
+'use strict'
+
+/**
+ * validateToken validates the specified access token.
+ * @param  {String} token       Access Token.
+ * @param  {Number} maxAge      The lifetime of an access token.
+ * @return {Promise}            Resolved if token is still valid, rejected
+ *                              otherwise.
+ */
+module.exports = (token, maxAge) => {
+  const created = new Date(token.created)
+  let expire = new Date(token.created)  
+  expire.setSeconds(expire.getSeconds() + maxAge)
+  return (expire > created) ? Promise.resolve() : Promise.reject()
+}
