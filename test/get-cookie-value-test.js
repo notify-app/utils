407f666891a0c9a8f026472da5d4382329c1c470
Luca Tabone <taboneluca3@gmail.com>
2017-05-14 09:56:35 +0200

    Refactor & fix test cases
    Apart from the small refactoring the test case that expected a
    rejected promise has been fixed. The problem with the current
    implementation of such test cases is that the test case would be
    failed due to timeout.

--- test/get-cookie-value-test.js
+++ test/get-cookie-value-test.js
@@ -5,9 +5,9 @@ const utils = require('../index')
 
 describe('utils.getCookieValue() method:', function () {
   describe('Scenario: Retrieving a cookie value from a cookie string containing info about one cookie:', function () {
-    let cookieString = null
-
     describe('Given a cookie string which contains info about one cookie,', function () {
+      let cookieString = null
+
       beforeEach(function () {
         cookieString = 'name=luca'
       })
@@ -17,9 +17,7 @@ describe('utils.getCookieValue() method:', function () {
 
         beforeEach(function () {
           return utils.getCookieValue(cookieString, 'name')
-            .then(value => {
-              cookieValue = value
-            })
+            .then(value => { cookieValue = value })
         })
 
         it('should retrieve the value of the specified cookie', function () {
@@ -30,11 +28,11 @@ describe('utils.getCookieValue() method:', function () {
   })
 
   describe('Scenario: Retrieving a cookie value from a cookie string containing info about multiple cookies:', function () {
-    let cookieString = null
-
     describe('Given a cookie string which contains info about multiple cookies,', function () {
+      let cookieString = null
+
       beforeEach(function () {
-        cookieString = 'name=luca; surname=tabone'
+        cookieString = 'id=1; name=luca; surname=tabone'
       })
 
       describe('when trying to retrieve the value of a cookie:', function () {
@@ -42,9 +40,7 @@ describe('utils.getCookieValue() method:', function () {
 
         beforeEach(function () {
           return utils.getCookieValue(cookieString, 'name')
-            .then(value => {
-              cookieValue = value
-            })
+            .then(value => { cookieValue = value })
         })
 
         it('should retrieve the value of the specified cookie', function () {
@@ -54,17 +50,18 @@ describe('utils.getCookieValue() method:', function () {
     })
   })
 
-  describe('Scenario: Retrieving a cookie value of a non-existant cookie:', function () {
-    let cookieString = null
-
+  describe('Scenario: Retrieving a cookie value of a non-existent cookie:', function () {
     describe('Given a cookie string,', function () {
+      let cookieString = null
+
       beforeEach(function () {
         cookieString = 'name=luca'
       })
 
-      describe('when trying to retrieve the value of a non-exitant cookie:', function () {
+      describe('when trying to retrieve the value of a non-existent cookie:', function () {
         it('should return a rejected promise', function (done) {
           utils.getCookieValue(cookieString, 'surname')
+            .then(() => done('Expected a rejected promise'))
             .catch(() => done())
         })
       })
