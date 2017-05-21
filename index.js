61167ea0805e9d9a56c3ae46f92436dc103e84d9
Luca Tabone <taboneluca3@gmail.com>
2017-05-21 17:02:04 +0200

    Update comments to comply with JSDOC

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 'use strict'
 
+/**
+ * This module contains common utilities that are used throughout the Notify
+ * project.
+ * @module notify-utils
+ * @borrows module:notify-utils/validate-token as validateToken
+ * @borrows module:notify-utils/get-cookie-value as getCookieValue
+ * @borrows module:notify-utils/get-user-by-token as getUserByToken
+ * @borrows module:notify-utils/get-token-from-request as getTokenFromRequest
+ */
 module.exports = {
   validateToken: require('./src/validate-token'),
   getCookieValue: require('./src/get-cookie-value'),
